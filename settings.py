dcf3c6b5ccc2988f36a7e88e4aaf25387c60d9ff
redebian <cyberreki@gmail.com>
2014-03-10 00:45:25 +0700

    test ignore 3

--- settings.py
+++ settings.py
@@ -6,21 +6,11 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-     ('redebian', 'cyberreki@gmail.com'),
+     (),
 )
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_tutorial',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'rinjani',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 TIME_ZONE = ''
 LANGUAGE_CODE = 'en-us'
